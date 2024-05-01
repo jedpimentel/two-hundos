967ba6bad157e546a4be707e8ae2748f714768f4
Jose E. Pimentel <jedpimentel@gmail.com>
2024-05-01 05:52:18 -0400

    make frontend apollo link uri relative, like a reasonable human would

--- frontend/src/appolloClient.js
+++ frontend/src/appolloClient.js
@@ -3,7 +3,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 const client = new ApolloClient({
   link: new HttpLink({
     // uri: 'http://localhost:4000/graphql', // Adjust this URI to match your server
-    uri: process.env.REACT_APP_GRAPHQL_URI, // Dynamically set based on the environment
+    // uri: process.env.REACT_APP_GRAPHQL_URI, // Dynamically set based on the environment
+    uri: '/graphql', // let's go humans!
   }),
   cache: new InMemoryCache(),
 });
