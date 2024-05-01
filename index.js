fb35fc35514af0e238c3bfb1842983e6465616a7
Jose E. Pimentel <jedpimentel@gmail.com>
2024-05-01 03:23:32 -0400

    init apollo server

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Two Hundos!');
-});
+// Define your schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// server.applyMiddleware({ app, path: '/graphql' });
+
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Two Hundos!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+async function startServer() {
+  const app = express();
+
+  // Create a new instance of ApolloServer
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  // Start Apollo Server
+//   await server.start();  // Make sure to await the start method
+  try {
+    await server.start();
+    // server.applyMiddleware({ app });
+    server.applyMiddleware({ app, path: '/graphql' });
+  } catch (error) {
+    console.error('Failed to start the Apollo Server', error);
+  }
+
+  // Apply middleware to the Express application
+//   server.applyMiddleware({ app });
+//   server.applyMiddleware({ app, path: '/graphql' });
+
+  // Start the Express server
+  app.listen({ port: PORT }, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+startServer();
